136af9af75b0a7843aac9da330e7e9d904ac4207
Tony <antonybrunoxyz@gmail.com>
2026-05-15 13:06:05 -0500

    agregar cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.chat import router as chat_router
 
@@ -12,4 +13,14 @@ app = FastAPI(
     title="StarTraining ChatBot API"
 )
 
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# rutas
 app.include_router(chat_router)
